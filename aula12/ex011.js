5589be215d5ad6688cfb66ca6cbca08f4c3ef6f1
Nathan Rodrigo <43752250+nrodrigoc@users.noreply.github.com>
2019-08-28 22:20:18 -0300

    Day_4

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 21
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log(`Não precisa votar!`)
+}else if(idade < 18){
+    console.log(`Pode votar!`)
+}else{
+    console.log(`Precisa votar!`)
+}
